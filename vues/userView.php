7593a4dc20dc4955cb358118ac1bb0fcfaf5a3aa
hugo0117 <rueos.kenamax@gmail.com>
2018-12-05 09:52:37 +0100

    userView et début de adminControl

--- vues/userView.php
+++ vues/userView.php
@@ -6,11 +6,18 @@
 <body>
 
 <?php
-    if (isset($tabNews)) {
-        foreach ($tabNews as $rowTabNews){
-            echo "<p>".$rowTabNews->getTitle()."</p>";
-        }
-    }
+	define('__ROOT__', dirname(dirname(__FILE__)));
+	require_once(__ROOT__.'/modeles/News.php'); 
+	require_once()
+	if(isset($dVueErreur)&&count($dVueErreur)>0){
+		echo '<h1> ERREUR CHARGEMENT DES NEWS </h1>'
+		foreach($dVueErreur as $value){
+			echo $value;
+		}
+	}
+	foreach($tabNews as $new){
+		echo '<a href ="index.php ?action=clickNews". '$new->getAddress()'.'$new->getTitle()'></a> </br>'
+	}
 ?>
 
 </body>
